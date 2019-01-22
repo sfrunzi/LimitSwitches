a3111ce860c0adcb114cc721a5da6e73263b1e41
Sara Frunzi <46463686+sfrunzi@users.noreply.github.com>
2019-01-22 14:08:29 -0500

    Update robot.java

--- robot.java
+++ robot.java
@@ -1,37 +1,30 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
+import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix.motorcontrol.ControlMode;;
 
 
-/**
- * This is a demo program showing the use of the RobotDrive class, specifically
- * it contains the code necessary to operate a robot with tank drive.
- */
 public class Robot extends TimedRobot {
-  private Joystick leftStick;
+  // private Joystick leftStick;
   private TalonSRX motor;
-  DigitalInput limitSwitchZero;
   DigitalInput limitSwitchOne;
   DigitalInput limitSwitchTwo;
   DigitalInput limitSwitchThree;
+  private Joystick controlPanel;
+
 
   @Override
   public void robotInit() {
-    leftStick = new Joystick(0);
+    // leftStick = new Joystick(0);
     motor = new TalonSRX(0);
 
-    limitSwitchZero = new DigitalInput(0);
     limitSwitchOne = new DigitalInput(1);
     limitSwitchTwo = new DigitalInput(2);
     limitSwitchThree = new DigitalInput(3);
@@ -39,27 +32,44 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
-   motor.set(ControlMode.PercentOutput, leftStick.getX());
+   // motor.set(ControlMode.PercentOutput, leftStick.getX());
+   limitSwitches();
+
+   ShuffleboardTab shuffleTab = Shuffleboard.getTab("Main");         // says what tab we're dealing with
+   NetworkTableEntry buttonValue = shuffleTab.add("button", true).getEntry();    // makes a button exist. If button is pressed, true. If not pressed, false.
+   boolean  value = buttonValue.getBoolean(false); // turns button into boolean, if above line doesn't give true/false, converts to false.
+   System.out.println(value);
   }
   public void limitSwitches() {
-      // level zero
-        if (limitSwitchZero.get() && limitSwitchOne.get()==false && limitSwitchTwo.get()==false && limitSwitchThree.get()==false){
-         System.out.println("level zero");
-      }
+    
       // level one
-        else if (limitSwitchZero.get()==false && limitSwitchOne.get() && limitSwitchTwo.get()==false && limitSwitchThree.get()==false){
+        if (limitSwitchOne.get() && limitSwitchTwo.get()==false && limitSwitchThree.get()==false){
           System.out.println("level one");
         }
       //level two
-       else if (limitSwitchZero.get()==false && limitSwitchOne.get() && limitSwitchTwo.get() && limitSwitchThree.get()==false) {
+       else if (limitSwitchOne.get() && limitSwitchTwo.get() && limitSwitchThree.get()==false) {
           System.out.println("level two");
         }
       // level three
-        else if (limitSwitchZero.get()==false && limitSwitchOne.get() && limitSwitchTwo.get() && limitSwitchThree.get()){
+        else if (limitSwitchOne.get() && limitSwitchTwo.get() && limitSwitchThree.get()){
           System.out.println("level three");
         }
         else {
           System.out.println("error");
         }
-    }
+        controlPanel = new Joystick(0);
+       // This is for the elevator up button
+        if (controlPanel.getRawButton(2)) {
+          System.out.println("up button");
+          motor.set(ControlMode.PercentOutput, 1);
+        }
+        // This is for the elevator down button
+        else if (controlPanel.getRawButton(3)){
+          System.out.println("down button");
+          motor.set(ControlMode.PercentOutput, -1);
+        }
+        else {
+          motor.set(ControlMode.PercentOutput, 0);
+        }
+    }   
   }
